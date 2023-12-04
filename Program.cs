47b960abedd30c4a6aa576cb37ed4dadbfc0e3bc
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-12-04 21:18:47 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Compute;
+
+// 1. Azure Authorization
+ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+
+// 2. Get the ResourceGroup
+string rgName = "myRG";
+ResourceGroupResource resourceGroup = await subscription.GetResourceGroups().GetAsync(rgName);
+
+// 3. Get the virtual machine collection from the resource group
+VirtualMachineCollection vmCollection = resourceGroup.GetVirtualMachines();
+string vmName = "myVM";
+VirtualMachineResource vm = await vmCollection.GetAsync(vmName);
+
+// 4. We delete the Virtual Machine
+await vm.DeleteAsync(WaitUntil.Completed);
